71509e9bbf08edf3bbe2140acb9cf089cc62f6d7
Thomas Aven <avensin@hotmail.com>
2019-03-04 17:31:44 +0100

    Add peak detection example.

--- analysis.py
+++ analysis.py
@@ -2,6 +2,7 @@ import experiment as expmnt
 import channelconverter as chconv
 import numpy as np
 import matplotlib.pyplot as plt
+import lib.detect_peaks as dp
 
 
 def split(data, sample_rate, ms):
@@ -29,7 +30,7 @@ def main():
     ch = chconv.MCSChannelConverter.mcsviz_to_channel[21]
     experiment = expmnt.Experiment('mea_data/1.h5')
 
-    # Example for bucketing data, here plotting the 13th bucket
+    """Example for bucketing data, here plotting the 13th bucket."""
     # (giving the 12th second).
     # ch_data, unit = experiment.get_channel_data(ch)
     # ch_data = split(ch_data, experiment.sample_rate, 1000)
@@ -37,9 +38,15 @@ def main():
     # plt.plot(ch_data[12])
     # plt.show()
 
-    # Examples for plotting.
+    """Examples for plotting."""
     # experiment.plot_channel(ch, start=11.8, end=12.8)
     # experiment.plot_channels(start=10, end=11)
+
+    """Peak detection example."""
+    # ch_data, unit = experiment.get_channel_data(ch)
+    # ch_data = split(ch_data, experiment.sample_rate, 1000)
+    # ch_data = ch_data[12]
+    # dp.detect_peaks(ch_data, show=True)
 
 
 if __name__ == '__main__':
