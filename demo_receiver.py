318821dee6ad69701ef94159aa77e3aea49ca330
Thomas Aven <avensin@hotmail.com>
2019-03-20 13:57:59 +0100

    Done demo.

--- demo_receiver.py
+++ demo_receiver.py
@@ -9,14 +9,29 @@ import numpy as np
 import serial
 import sthread
 import time
+import meamer
 
 
 sensor_distance = 1500
+sensor_distances = []
+is_object_close = False
+
+
 def receive_sensor():
     global sensor_distance
+    global sensor_distances
+    global is_object_close
+
     with serial.Serial('/dev/ttyUSB5') as ser:
         while True:
             sensor_distance = serial_distance(ser)
+            sensor_distances.append(1.0 if sensor_distance < 100.0 else 0.0)
+            sensor_distances = sensor_distances[-10:]
+
+            if np.mean(sensor_distances) >= 0.6:
+                is_object_close = True
+            else:
+                is_object_close = False
 
             if sthread.check_terminate_thread():
                 return
@@ -59,9 +74,11 @@ def serial_distance(serial_connection):
 
 
 def run_demo(connection):
+    meame = meamer.MEAMEr('10.20.92.130')
     SMA_amplitude_threshold = 1e-5
     peak_amplitude_threshold = 7.3e-5
     previous_predictions = []
+    previous_object_state = False
     sensor_thread = sthread.StoppableThread(target=receive_sensor)
     sensor_thread.start()
 
@@ -82,8 +99,16 @@ def run_demo(connection):
             if (np.mean(previous_predictions) >= 0.5):
                 print('seriously epic win')
 
-            if sensor_distance < 100:
-                print('you are very close >:(')
+            if previous_object_state != is_object_close:
+                previous_object_state ^= True
+
+                if previous_object_state:
+                    meame.start_stim()
+                    meame.setup_stim()
+                    print('Started remote MEAME stimuli')
+                else:
+                    meame.stop_stim()
+                    print('Stopped remote MEAME stimuli')
     except KeyboardInterrupt as e:
         sensor_thread.stop()
 
